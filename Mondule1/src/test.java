31a64c89968c0c2ea698412b5ad0d78a892f0a65
ZGHOME <919892645@qq.com>
2020-08-29 23:03:36 +0800

    Begin study Java

---
+++ Mondule1/src/test.java
@@ -0,0 +1,24 @@
+/**
+ * @author zg
+ * @version 1.0
+ * @since 1.8
+ */
+public class test {
+    public static void main(String[] args) {
+        /*int pi = (int) Math.PI;
+        System.out.println(pi);
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b:" + (a && b));*/
+        String a = test1();
+        System.out.println(a);
+        String aa = "1";
+        if (aa == "1"){
+            System.out.println(aa);
+        }
+    }
+    public static String test1(){
+
+        return "Hello";
+    }
+}
